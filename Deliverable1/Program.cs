8b188dfb32e2e702e9bcc522a2b66f32b8900b22
Matthew Miller <ginger424901@gmail.com>
2018-01-12 00:05:29 -0500

    fixed bug.

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -6,6 +6,7 @@ namespace Deliverable1
     {
         static void Main(string[] args)
         {
+            //gets first number
             Console.Write("Enter the first number: ");
             var strNum1 = Console.ReadLine();
             int num1;
@@ -16,6 +17,7 @@ namespace Deliverable1
                 return;
             }
 
+            //gets second number
             Console.Write("Enter the second number: ");
             if (!int.TryParse(Console.ReadLine(), out int num2))
             {
@@ -23,33 +25,41 @@ namespace Deliverable1
                 return;
             }
 
+
             bool doTheNumbersMatch = DoNumbersMatch(num1, num2);
 
+            //writes out answer
             Console.WriteLine(doTheNumbersMatch ? "Those numbers match" : "Those numbers do not match");
+            Console.ReadKey();
         }
 
         private static bool DoNumbersMatch(int num1, int num2)
         {
+            //converts numbers to array
             var num1ToArray = num1.ToString().ToCharArray();
             var num2ToArray = num2.ToString().ToCharArray();
 
+            //tests if numbers are the same length
             if (num1ToArray.Length != num2ToArray.Length)
             {
                 return false;
             }
 
+            //sets the index for the array
             int answer = 0;
             for (var i = 0; i < num1ToArray.Length && i < num2ToArray.Length; i++)
             {
                 var firstNum = int.Parse(num1ToArray[i].ToString());
                 var secondNum = int.Parse(num2ToArray[i].ToString());
 
+                //tests first set of numbers
                 if (i == 0)
                 {
                     answer = firstNum + secondNum;
                     continue;
                 }
 
+                //test remaining numbers against the first set of number
                 if (answer != (firstNum + secondNum))
                 {
                     return false;
